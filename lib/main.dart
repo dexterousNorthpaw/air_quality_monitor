155382da8312a802a642f7a5b547d27ece35c40f
Undie Ebenezer <ebenezerundie@gmail.com>
2023-02-08 00:06:41 +0100

    chore: navigate to login page first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:air_quality_monitor/providers/data_provider.dart';
 import 'package:air_quality_monitor/screens/loginScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,26 +21,24 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Air Quality Monitor',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => DataProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Air Quality Monitor',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: GestureDetector(
+          child: const LoginScreen(),
+        ),
       ),
-      home: GestureDetector(child: const LoginScreen()),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
