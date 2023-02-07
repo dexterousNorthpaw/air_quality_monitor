3d620dba76e62206a39f4bea0be814073d5ca1de
Undie Ebenezer <ebenezerundie@gmail.com>
2023-02-08 00:09:33 +0100

    feat: persists email after login

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,10 +1,11 @@
 import 'package:air_quality_monitor/screens/homeScreen.dart';
-import 'package:air_quality_monitor/services/firebase.dart';
+import 'package:air_quality_monitor/services/firebase_auth.dart';
 import 'package:air_quality_monitor/shared/custom_button.dart';
 import 'package:air_quality_monitor/shared/login_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../shared/popup_dialog.dart';
 
@@ -20,8 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _passwordController = TextEditingController();
   bool showPassword = false;
   final _formKey = GlobalKey<FormState>();
-  final _emailKey = GlobalKey();
-  final _passwordKey = GlobalKey();
+  late final SharedPreferences _preferences;
 
   togglePasswordVisibility() {
     setState(() {
@@ -31,16 +31,19 @@ class _LoginScreenState extends State<LoginScreen> {
 
   login({required String email, required String password}) async {
     final data =
-        await FirebaseServices().login(email: email, password: password);
+        await FirebaseAuthServices.login(email: email, password: password);
 
     if (data['status']) {
-      // await storage.write(key: 'password', value: userPassword);
+      await _preferences.setString('email', email);
       if (mounted) {
-        Navigator.pushReplacement(
+        Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(
             builder: (context) => const HomeScreen(),
           ),
+          (route) {
+            return false;
+          },
         );
       }
     } else {
@@ -50,6 +53,19 @@ class _LoginScreenState extends State<LoginScreen> {
         cancelText: "Close",
       );
     }
+  }
+
+  initializeData() async {
+    _preferences = await SharedPreferences.getInstance();
+    String? email = _preferences.getString('email');
+    if (email == null) return;
+    _emailController.text = email;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeData();
   }
 
   @override
@@ -68,92 +84,85 @@ class _LoginScreenState extends State<LoginScreen> {
         appBar: AppBar(
           toolbarHeight: 60,
           centerTitle: true,
-          title: const Text(
+          title: Text(
             'Air Quality Monitor',
-            style: TextStyle(
-              color: Color(0xff834eed),
-              fontSize: 23,
-              fontWeight: FontWeight.w800,
+            style: GoogleFonts.montserrat(
+              textStyle: const TextStyle(
+                color: Color(0xff834eed),
+                fontSize: 20,
+                fontWeight: FontWeight.w800,
+              ),
             ),
           ),
           backgroundColor: Colors.white,
         ),
         body: LayoutBuilder(builder: (context, constraint) {
-          return SingleChildScrollView(
-            padding: EdgeInsets.only(
-                bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Container(
-              margin: const EdgeInsets.fromLTRB(20, 25, 20, 40),
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              height: constraint.maxHeight - 65,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Login',
-                          style: GoogleFonts.montserrat(
-                            textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 1,
+          return ScrollConfiguration(
+            behavior: const ScrollBehavior().copyWith(overscroll: false),
+            child: SingleChildScrollView(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(20, 25, 20, 40),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                height: constraint.maxHeight - 65,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Login',
+                            style: GoogleFonts.montserrat(
+                              textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 1,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 40),
-                        LoginFields(
-                          fieldName: 'Email',
-                          textEditingController: _emailController,
-                          hintText: 'Enter your email',
-                          key: _emailKey,
-                          onTapTextField: () async {
-                            await Future.delayed(
-                                const Duration(milliseconds: 200));
-                            Scrollable.ensureVisible(_emailKey.currentContext!);
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        LoginFields(
-                          fieldName: 'Password',
-                          hintText: 'Enter your password',
-                          textEditingController: _passwordController,
-                          obscureText: !showPassword,
-                          onPressed: togglePasswordVisibility,
-                          key: _passwordKey,
-                          onTapTextField: () async {
-                            await Future.delayed(
-                                const Duration(milliseconds: 200));
-                            Scrollable.ensureVisible(
-                                _passwordKey.currentContext!);
-                          },
-                        ),
-                        const SizedBox(height: 60),
-                        CustomButton(
-                          buttonText: 'Login',
-                          onTap: () async {
-                            FocusScope.of(context).unfocus();
-                            await SystemChannels.textInput
-                                .invokeMethod('TextInput.hide');
-                            if (_formKey.currentState!.validate()) {
-                              await login(
+                          const SizedBox(height: 40),
+                          LoginFields(
+                            fieldName: 'Email',
+                            textEditingController: _emailController,
+                            hintText: 'Enter your email',
+                          ),
+                          const SizedBox(height: 20),
+                          LoginFields(
+                            fieldName: 'Password',
+                            hintText: 'Enter your password',
+                            textEditingController: _passwordController,
+                            obscureText: !showPassword,
+                            onPressed: togglePasswordVisibility,
+                          ),
+                          const SizedBox(height: 60),
+                          CustomButton(
+                            buttonText: 'Login',
+                            onTap: () async {
+                              FocusScope.of(context).unfocus();
+                              await SystemChannels.textInput
+                                  .invokeMethod('TextInput.hide');
+                              if (_formKey.currentState!.validate()) {
+                                await login(
                                   email: _emailController.text.trim(),
-                                  password: _passwordController.text.trim());
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                                  password: _passwordController.text.trim(),
+                                );
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
